a5f428ddc47bc0a9d5c48c0eba19806f369f1447
Isham Faizal <ishamf@users.noreply.github.com>
2015-08-11 22:37:56 +0700

    Make this plugin work with any standard version

--- domain.js
+++ domain.js
@@ -1,42 +1,121 @@
-var exec = require('child_process').exec
+var cpExec = require('child_process').exec
 var path = require('path')
+var fs = require('fs')
 var Promise = require('bluebird')
 
-var getStandard = (function () {
-  var standard = null
-  var stdPromise = new Promise(function (resolve, reject) {
-    // Yes, it's a hack. Please send a PR if you have a better way.
-    exec('npm root -g', function (error, stdout, stderr) {
-      if (error) {
-        reject(error)
+var fileExists = Promise.promisify(fs.exists)
+
+function exec (cmd) {
+  return new Promise(function (resolve, reject) {
+    cpExec(cmd, function (err, stdout) {
+      if (err) {
+        reject(err)
       } else {
-        try {
-          resolve(require(path.join(stdout.trim(), 'standard')))
-        } catch (e) {
-          reject(e)
-        }
+        resolve(stdout)
       }
     })
-  }).then(function (s) {
-    standard = s
-    return s
   })
+}
 
-  return function () {
-    if (standard) {
-      return Promise.resolve(standard)
+var getStandard = (function () {
+  var standardVersions = {}
+  var projectStandardVersions = {}
+
+  function projectStandardExists (projectRoot) {
+    if (projectStandardVersions[projectRoot]) {
+      return Promise.resolve(true)
     } else {
-      return stdPromise
+      return fileExists(path.join(projectRoot, 'node_modules', 'standard', 'package.json'))
+    }
+  }
+
+  function getProjectStandardVersion (projectRoot) {
+    return require(path.join(projectRoot, 'node_modules', 'standard', 'package.json')).version
+  }
+
+  var getGlobalStandard = (function () {
+    var globalStandardVersion = null
+
+    return function () {
+      var p
+
+      if (!globalStandardVersion) {
+        // Yes, it's a hack. Please send a PR if you have a better way.
+        p = exec('npm root -g')
+          .then(function (stdout) {
+            var globalStandardPath = path.join(stdout.trim(), 'standard')
+            globalStandardVersion = require(path.join(globalStandardPath, 'package.json')).version
+
+            if (!standardVersions[globalStandardVersion]) {
+              standardVersions[globalStandardVersion] = require(globalStandardPath)
+            }
+
+            return standardVersions[globalStandardVersion]
+          })
+      } else {
+        p = Promise.resolve(standardVersions[globalStandardVersion])
+      }
+      return p.then(function (std) {
+        return {
+          engine: std,
+          version: globalStandardVersion
+        }
+      })
+    }
+  })()
+
+  function getProjectStandard (projectRoot) {
+    return projectStandardExists(projectRoot)
+    .then(function (projectHasStandard) {
+      if (!projectHasStandard) {
+        return getGlobalStandard()
+      }
+
+      if (!projectStandardVersions[projectRoot]) {
+        projectStandardVersions[projectRoot] = getProjectStandardVersion(projectRoot)
+      }
+
+      var projectStandardVersion = projectStandardVersions[projectRoot]
+
+      if (!standardVersions[projectStandardVersion]) {
+        standardVersions[projectStandardVersion] = require(path.join(projectRoot, 'node_modules', 'standard'))
+      }
+
+      return {
+        engine: standardVersions[projectStandardVersion],
+        version: projectStandardVersion
+      }
+    })
+  }
+
+  return function (projectRoot) {
+    if (projectRoot) {
+      return getProjectStandard(projectRoot)
+    } else {
+      return getGlobalStandard()
     }
   }
 })()
 
-function lintText (textContent, cb) {
-  getStandard()
+function lintText (args, cb) {
+  var projectRoot = args[0]
+  var textContent = args[1]
+
+  getStandard(projectRoot)
   .then(function (standard) {
-    standard.lintText(textContent, {}, cb)
+    console.log(standard)
+    standard.engine.lintText(textContent, {}, function (error, result) {
+      if (error) {
+        cb(error)
+      } else {
+        result.version = standard.version
+        cb(null, result)
+      }
+
+    })
   })
   .catch(function (e) {
+    console.log(e)
     cb(e)
   })
 }
--- main.js
+++ main.js
@@ -3,10 +3,14 @@
 define(function (require, exports, module) {
   'use strict'
 
+  console.log('Live!')
+
   var CodeInspection = brackets.getModule('language/CodeInspection')
   var LanguageManager = brackets.getModule('language/LanguageManager')
   var NodeDomain = brackets.getModule('utils/NodeDomain')
   var ExtensionUtils = brackets.getModule('utils/ExtensionUtils')
+  var ProjectManager = brackets.getModule('project/ProjectManager')
+  var semverGte = require('semver').gte
 
   var JS_LANGUAGE = LanguageManager.getLanguageForExtension('js')
 
@@ -14,14 +18,14 @@ define(function (require, exports, module) {
 
   function scanFileAsync (textContent) {
     var d = $.Deferred()
-    standardDomain.exec('lintText', textContent)
+    standardDomain.exec('lintText', [ProjectManager.getProjectRoot().fullPath, textContent])
       .done(function (result) {
         d.resolve({
           errors: result.results[0].messages.map(function (msg) {
             return {
               pos: {
                 line: msg.line - 1,
-                ch: msg.column
+                ch: msg.column + (semverGte(result.version, '5.0.0') ? -1 : 0)
               },
               message: msg.message,
               type: msg.severity === 1 ? CodeInspection.Type.WARNING
